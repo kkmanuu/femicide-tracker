8f49ab6e58f7c688d735790cdc323c9d08c3aaac
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-03-29 03:14:01 +0300

    Configure proxy middleware for API routes

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/users',
+    createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true })
+  );
+  app.use(
+    '/femicides',
+    createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true })
+  );
+};
